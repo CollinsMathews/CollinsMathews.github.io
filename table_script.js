b834929b6a03e649c60444979f49e9bbc48a9859
Collins <cmat0007@student.monash.edu>
2019-03-16 16:32:06 +1100

    Resizing table

--- table_script.js
+++ table_script.js
@@ -5,16 +5,16 @@ var shift_times = ['8:00AM - 12:00M', '10:00PM - 2:00PM', '12:00PM - 4:00PM', '2
 
 document.getElementById('shift_table').innerHTML += '<thead><tr>';
 for (var i = 0; i < 8; i++) {
-    shift_table_string += '<td>' + days_of_week[i] + '</td>';
+    shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + days_of_week[i] + '</th>';
 }
 
-shift_table_string += '</tr>';
+shift_table_string += '</tr></thead>';
 
 
 for (var i = 0; i < 4; i++) {
-    shift_table_string += '<tr><td>' + shift_times[i] + '</td>';
+    shift_table_string += '<tr id="row_' + (i+1) + '"><td class="shift_times_cell">' + shift_times[i] + '</td>';
     for (var j = 0; j < 7; j++) {
-        shift_table_string += '<td>' + '' + '</td>';
+        shift_table_string += '<td class="shift_cell">' + '' + '</td>';
     }
     shift_table_string += '</tr>';
 }
