3f53c4ce85b3321995f106631f09d4e96e1da3e9
jwan0042 <jwan0042@student.monash.edu>
2019-03-17 00:46:03 +1100

    newww

--- scripts/employer_page.js
+++ scripts/employer_page.js
@@ -2,11 +2,11 @@
 shift_table_string = '';
 
 
-var days_of_week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+var days_of_week = ['', 'Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 var shift_times = ['8:00AM - 12:00PM', '10:00PM - 2:00PM', '12:00PM - 4:00PM', '2:00PM - 6:00PM'];
 
 document.getElementById('shift_table').innerHTML += '<thead><tr>';
-for (var i = 0; i < days_of_week.length + 1; i++) {
+for (var i = 0; i < days_of_week.length; i++) {
   if (i == 0) {
     shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + '<form action="#">\
         <div class="mdl-textfield mdl-js-textfield">\
@@ -15,7 +15,7 @@ for (var i = 0; i < days_of_week.length + 1; i++) {
         </div>\
       </form>' + '</th>';
   } else {
-    shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + days_of_week[i - 1] + '</th>';
+    shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + days_of_week[i] + '</th>';
   }
 }
 
@@ -24,10 +24,10 @@ shift_table_string += '</tr></thead>';
 
 for (var i = 0; i < shift_times.length; i++) {
   shift_table_string += '<tr id="row_' + (i) + '"><td class="shift_times_cell">' + shift_times[i] + '</td>';
-  for (var j = 0; j < days_of_week.length; j++) {
-    shift_table_string += '<td class="shift_cell">' + '<form action="#" id="credit_input">\
+  for (var j = 0; j < days_of_week.length - 1; j++) {
+    shift_table_string += '<td class="employer_shift_cell">' + '<form action="#" id="preferences_output">\
         <div class="mdl-textfield mdl-js-textfield">\
-        <input id="' + days_of_week[j] + '_' + shift_times[i] + '" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">\
+        <input id="' + days_of_week[j + 1] + '_' + shift_times[i] + '" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">\
         <label class="mdl-textfield__label" for="sample2">Credits...</label>\
         </div>\
         </form>' + '</td>';
