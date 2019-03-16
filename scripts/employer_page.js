759541c5f550bb6d5f65a5bee79ff44edb68fa59
Changas406 <Changas406@users.noreply.github.com>
2019-03-17 00:51:04 +1100

    change hard code AGAIN

--- scripts/employer_page.js
+++ scripts/employer_page.js
@@ -2,15 +2,15 @@
 shift_table_string = '';
 
 
-var days_of_week = ['', 'Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+var days_of_week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 var shift_times = ['8:00AM - 12:00PM', '10:00PM - 2:00PM', '12:00PM - 4:00PM', '2:00PM - 6:00PM'];
 
 document.getElementById('shift_table').innerHTML += '<thead><tr>';
-for (var i = 0; i < days_of_week.length; i++) {
+for (var i = 0; i < days_of_week.length+1; i++) {
   if (i == 0) {
     shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + '</th>';
   } else {
-    shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + days_of_week[i] + '</th>';
+    shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + days_of_week[i-1] + '</th>';
   }
 }
 
@@ -19,7 +19,7 @@ shift_table_string += '</tr></thead>';
 
 for (var i = 0; i < shift_times.length; i++) {
   shift_table_string += '<tr id="row_' + (i) + '"><td class="shift_times_cell">' + shift_times[i] + '</td>';
-  for (var j = 0; j < days_of_week.length - 1; j++) {
+  for (var j = 0; j < days_of_week.length; j++) {
     shift_table_string += '<td class="shift_cell">' + 'Testies' + '</td>';
   }
   shift_table_string += '</tr>';
