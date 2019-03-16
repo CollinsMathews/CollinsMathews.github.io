0e817dc3b7b809bea8770586d303a5ac71a6aa10
Collins <collins199@gmail.com>
2019-03-17 09:52:49 +1100

    4 to 3

--- scripts/employee_page.js
+++ scripts/employee_page.js
@@ -37,7 +37,7 @@ function handleClientLoad() {
 
 
 var days_of_week = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun'];
-var shift_times = ['8:00AM - 11:00AM', '11:00AM - 2:00PM', '2:00PM - 5:00PM', '5:00PM - 8:00PM', '8:00PM - 11:00PM'];
+var shift_times = ['8:00AM - 11:00AM', '11:00AM - 2:00PM', '2:00PM - 5:00PM'];
 
 document.getElementById('shift_table').innerHTML += '<thead><tr>';
 for (var i = 0; i < days_of_week.length + 1; i++) {
