74b3b274aa13b6094325175ee22fe63b5af0c316
Changas406 <Changas406@users.noreply.github.com>
2019-03-16 22:43:21 +1100

    Connected pref array creation to monkres

--- scripts/algorithm.js
+++ scripts/algorithm.js
@@ -3,11 +3,7 @@ var test = [[0, 0.2, 0.3, 0.1],
         [0.1, 0.8, 0, 0.1],
         [0.6, 0.1, 0.2, 0.1]];
 
-        
-
-var matrix = [[1, 2, 3],
-              [1, 2, 3],
-              [1, 2, 3]];
+var matrix = pref_array;
 
 min_matrix = matrix.map(value => value.map(value => -value));
 
