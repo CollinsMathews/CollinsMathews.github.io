8079b2b5ad10ac7f8d7c79e4cefd3f49fcf2117d
Collins <cmat0007@student.monash.edu>
2019-03-17 00:24:17 +1100

    Fixed algorithm errors

--- scripts/employee_page.js
+++ scripts/employee_page.js
@@ -1,11 +1,11 @@
 shift_table_string = '';
 
 
-var days_of_week = ['', 'Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+var days_of_week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 var shift_times = ['8:00AM - 11:00AM', '11:00AM - 2:00PM', '2:00PM - 5:00PM', '5:00PM - 8:00PM'];
 
 document.getElementById('shift_table').innerHTML += '<thead><tr>';
-for (var i = 0; i < days_of_week.length; i++) {
+for (var i = 0; i < days_of_week.length + 1; i++) {
     if (i == 0) {
         shift_table_string += '<th id="days_cell" class="mdl-data-table__cell--non-numeric">' + '<form action="#">\
         <div class="mdl-textfield mdl-js-textfield">\
@@ -23,10 +23,10 @@ shift_table_string += '</tr></thead>';
 
 for (var i = 0; i < shift_times.length; i++) {
     shift_table_string += '<tr id="row_' + (i) + '"><td class="shift_times_cell">' + shift_times[i] + '</td>';
-    for (var j = 0; j < days_of_week.length - 1; j++) {
+    for (var j = 0; j < days_of_week.length; j++) {
         shift_table_string += '<td class="shift_cell">' + '<form action="#" id="credit_input">\
         <div class="mdl-textfield mdl-js-textfield">\
-        <input id="' + days_of_week[j + 1] + '_' + shift_times[i] + '" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">\
+        <input id="' + days_of_week[j] + '_' + shift_times[i] + '" class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="sample2">\
         <label class="mdl-textfield__label" for="sample2">Credits...</label>\
         </div>\
         </form>' + '</td>';
@@ -57,8 +57,8 @@ function onSubmit() {
     var credits_used;
     shift_array = [];
     for (var i = 0; i < shift_times.length; i++) {
-        for (var j = 0; j < days_of_week.length - 1; j++) {
-            var day_on = days_of_week[j + 1];
+        for (var j = 0; j < days_of_week.length ; j++) {
+            var day_on = days_of_week[j];
             var shift_time_on = shift_times[i];
 
             if (document.getElementById(day_on + '_' + shift_time_on) == undefined || (credits_used = document.getElementById(day_on + '_' + shift_time_on).value) == "") {
